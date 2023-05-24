ef190e0dc16d6afa643127a9c4b8f0ce8c3e1a95
Matheus Aguiar <107930729+Matheus-Souza-Aguiar@users.noreply.github.com>
2023-05-24 17:08:07 -0300

    Update reserva_de_carros.py

--- reserva_de_carros.py
+++ reserva_de_carros.py
@@ -1,12 +1,16 @@
 import datetime
-import pandas as pd
-import pyodbc
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
+
+
+app = FastAPI()
 
 host = 'localhost'
 database = 'ReservaCarros'
 username = 'postgres'
-pwd = 'AAAaaa123'
+pwd = 'root'
 port_id = '5432'
 
 conn = psycopg2.connect(
@@ -18,102 +22,50 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
+origins = ['http://127.0.0.1:5500'] 
 
-def car_registration():
-    print('Cadastrar veículo.\nTodos os campos devem ser prenchidos corretamente\n')
-    model = input('Por favor informe o modelo do carro?  ')
-    board = input('Informe a placa do carro?  ')
-    year = input('Informe o ano do carro? ')
-    state = input('O carro está disponivel ou na manutenção? Disponivel(d)/manutenção(m):  ')
-    if state == 'd':
-        state = 'Disponivel'
-    elif state == 'm':
-        state = 'Manutenção'
-    check = input(
-        f'\nO carro com as seguintes informações, Modelo: {model}, placa: {board}, ano: {year}, está {state}.\nVocê confirma essas informações? sim(s)/não(n)? ')
-    if check == 's':
-        print('Carro registrado')
-        registration = f"""INSERT INTO cars (modelCAr, boardCar, yearCar, stateCar) VALUES ('{model}', '{board}', '{year}', '{state}')"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+) 
+
+class Cars(BaseModel):
+    idcar: int = None
+    model: str = None
+    board: str = None
+    year:  str = None
+    state: str = 'disponivel' 
+
+class ReservedCar(BaseModel):
+    id_car: int = None
+    reserve_outset: str = None
+    reserve_last: str = None
+
+
+@app.post('/cadastro')
+def reservar(cars: Cars):    
+    
+    registration = f"""INSERT INTO cars (modelCAr, boardCar, yearCar, stateCar) VALUES ('{cars.model}', '{cars.board}', '{cars.year}', '{cars.state}')"""
+    
+    try:
         cursor.execute(registration)
         conn.commit()
-        return main()
-    elif check == 'n':
-        print('As infomações serão alteradas')
-        return car_registration()
+        return cars
+    except:
+        return {'Erro': 'Por favor conferir os dados do carro.'}
 
 
-def reserve_car():
-    print('Reserva de carros\n')
-    reserve_outset = input('Informe o dia e a hora em que você ira reservar o carro? DD/MM/YYY HH:MM: ')
-    reserve_last = input('Informe até que dia e hora pretende ter o carro reservado? DD/MM/YYY HH:MM: ')
-    confer = input(
-        f'\nVocê pretende iniciar sua reserva em {reserve_outset} e finalizar em {reserve_last}? sim(s)/não(n): ')
-    if confer == 's':
-        reserve_outset = datetime.datetime.strptime(reserve_outset, '%d/%m/%Y %H:%M')
-        reserve_last = datetime.datetime.strptime(reserve_last, '%d/%m/%Y %H:%M')
-        print('\nVamos verificar se tem vaga disponivel para essa data')
-        return check_cars_in_reverse(reserve_outset, reserve_last)
-    elif confer == 'n':
-        return reserve_car()
-
-
-def checkout(idcar, reserve_outset, reserve_last):
-    data_now = datetime.datetime.now()
-    check = f"""select reservedcar.reserveoutset, reservedcar.reservelast
-                 from cars
-                 INNER JOIN reservedcar
-                 ON  cars.idcar = reservedcar.idCar 
-				 WHERE 
-				 reservedcar.reservelast > '{data_now}'  AND 
-				 cars.idcar = {idcar}"""
-
-    list_true_false = []
-    cursor.execute(check)
-    list_car_reserve = cursor.fetchall()
-    for res in list_car_reserve:
-        if reserve_outset > res[0]:
-
-            if reserve_outset > res[1]:
-                true = True
-                list_true_false.append(true)
-
-            elif reserve_outset < res[1]:
-                false = False
-                list_true_false.append(false)
-
-        elif reserve_outset < res[0]:
-
-            if reserve_last < res[0]:
-                true = True
-                list_true_false.append(true)
-
-            elif reserve_last > res[0]:
-                false = False
-                list_true_false.append(false)
-
-        elif reserve_outset == res[0]:
-            true = False
-            list_true_false.append(true)
-
-        elif reserve_last == res[1]:
-            false = False
-            list_true_false.append(false)
-
-    if False in list_true_false:
-        print('Sem vaga para essa data')
-        return reserve_car()
-    else:
-        registration = f"""INSERT INTO Reservedcar(idcar, reserveOutset, reserveLast) VALUES ({idcar}, '{reserve_outset}', '{reserve_last}');"""
-        cursor.execute(registration)
-        conn.commit()
-        print('Sua reserva foi registrada!')
-        return final()
-
-
-def check_cars_in_reverse(reserve_outset, reserve_last):
-    reserve_cars = []
+@app.post('/consulta_reserva')
+def reserved_car(reservedCar: ReservedCar):
     list_True_False = []
     ids = []
+    reservedCar.reserve_outset = datetime.datetime.strptime(reservedCar.reserve_outset, '%d/%m/%Y %H:%M')
+    reservedCar.reserve_last = datetime.datetime.strptime(reservedCar.reserve_last, '%d/%m/%Y %H:%M')
+    
     data_now: datetime = datetime.datetime.now()
     check = f"""select reservedcar.idcar, cars.modelcar, reservedcar.reserveoutset, reservedcar.reservelast
                      from cars
@@ -124,174 +76,226 @@ def check_cars_in_reverse(reserve_outset, reserve_last):
     cursor.execute(check)
     list_car_reserve = cursor.fetchall()
     for res in list_car_reserve:
-        if reserve_outset > res[2]:
+        if reservedCar.reserve_outset > res[2]:
 
-            if reserve_outset > res[3]:
+            if reservedCar.reserve_outset > res[3]:
                 true = True
                 list_True_False.append(true)
 
-            elif reserve_outset < res[3]:
+            elif reservedCar.reserve_outset < res[3]:
+                
                 ids.append(res[0])
-                reserve_cars.append(res)
                 false = False
                 list_True_False.append(false)
 
 
-        elif reserve_outset < res[2]:
-            if reserve_last < res[2]:
+        elif reservedCar.reserve_outset < res[2]:
+            if reservedCar.reserve_last < res[2]:
                 true = True
                 list_True_False.append(true)
 
-            elif reserve_last > res[2]:
+            elif reservedCar.reserve_last > res[2]:
+                
                 ids.append(res[0])
-                reserve_cars.append(res)
                 false = False
                 list_True_False.append(false)
 
-        elif reserve_outset == res[2]:
+        elif reservedCar.reserve_outset == res[2]:
+            
             ids.append(res[0])
-            reserve_cars.append(res)
             false = False
             list_True_False.append(false)
 
-        elif reserve_last == res[3]:
-            reserve_cars.append(res)
+        elif reservedCar.reserve_last == res[3]:
             false = False
             list_True_False.append(false)
-
+   
+        
     ids1 = tuple((ids))
-
+    
     check = f"""select count (idcar)
                 from cars; """
     cursor.execute(check)
     list_car_reserve = cursor.fetchall()
     car = list_car_reserve[0]
     car1 = car[0]
+    
+    
     if len(ids1) == car1:
-        print('Sem vaga, Por favo, informe outra data')
-        return reserve_car()
+        return {'msg': 'Sem vaga, informe outra data'}
     else:
-        if False in list_True_False:
+      if False in list_True_False:
 
             if len(ids1) > 1:
-                option_cars_one(ids1, reserve_outset, reserve_last)
+                car = []
+                check = f"""SELECT cars.idcar, cars.modelcar, cars.yearcar
+                            FROM cars
+                            WHERE cars.idcar NOT IN {ids1} """
+                cursor.execute(check)
+                list_car_reserve = cursor.fetchall()
+                for cars in list_car_reserve:
+                    car.append(cars)
+                
+                return car
 
             elif len(ids1) == 1:
-                option_cars_two(ids1, reserve_outset, reserve_last)
-
-            elif len(ids1) == 0:
-                print('Não tem carro disponivel para essa data')
-                return reserve_car()
-        else:
-            option_cars_three(reserve_outset, reserve_last)
-
-
-def option_cars_one(ids1, reserve_outset, reserve_last):
-    print('Carros disponiveis para essa data')
-    check = pd.read_sql(f"""SELECT cars.idcar, cars.modelcar, cars.yearcar
-                            FROM cars
-                            WHERE cars.idcar NOT IN {ids1} """, conn)
-    print(check)
-    idcar = input('Informe o Id do carro que deseja reservar? ')
-    return check_id_car(idcar, ids1, reserve_outset, reserve_last)
-
-
-def option_cars_two(ids1, reserve_outset, reserve_last):
-    ent = ids1[0]
-    ids1 = int((ent))
-    print('Carros disponiveis para essa data')
-    check = pd.read_sql(f"""select cars.idcar, cars.modelcar, cars.yearcar
+                car = []
+                ent = ids1[0]
+                ids1 = int((ent))
+                check = f"""select cars.idcar, cars.modelcar, cars.yearcar
                             from cars
-                            WHERE cars.idcar <> {ids1} """, conn)
-    print(check)
-    idcar = input('Informe o Id do carro que deseja reservar? ')
-    return check_id_car_option_two(idcar, ids1, reserve_outset, reserve_last)
+                            WHERE cars.idcar <> {ids1} """
+                
+               
+                cursor.execute(check)
+                list_car_reserve = cursor.fetchall()
+                for cars in list_car_reserve:
+                    car.append(cars)
+                
+                return car
+                           
+            elif len(ids1) == 0:
+                return {'msg:' 'Sem vaga para essa data'}                      
+      
+      else:
+        car = []
+        check = """SELECT cars.idcar, cars.modelcar, cars.yearcar
+                                       FROM cars """
+        cursor.execute(check)
+        list_car_reserve = cursor.fetchall()
+        for cars in list_car_reserve:
+            car.append(cars)
+        
+        try:
+            return car
+        except:
+            return {'Erro': 'Verificar dados de reserva'}
+        
 
 
-def option_cars_three(reserve_outset, reserve_last):
-    print('Carros disponiveis para essa data')
-    check = pd.read_sql("""SELECT cars.idcar, cars.modelcar, cars.yearcar
-                                       FROM cars """, conn)
-    print(check)
-    idcar = input('Informe o Id do carro que deseja reservar? ')
-    return check_id_car_option_three(idcar, reserve_outset, reserve_last)
+@app.post('/faz_reserva')
+def checkout(reservedCar: ReservedCar):
+    
+    reservedCar.reserve_outset = datetime.datetime.strptime(reservedCar.reserve_outset, '%d/%m/%Y %H:%M')
+    reservedCar.reserve_last = datetime.datetime.strptime(reservedCar.reserve_last, '%d/%m/%Y %H:%M')
+    
+    data_now = datetime.datetime.now()
+    check = f"""select reservedcar.reserveoutset, reservedcar.reservelast
+                 from cars
+                 INNER JOIN reservedcar
+                 ON  cars.idcar = reservedcar.idCar 
+				 WHERE 
+				 reservedcar.reservelast > '{data_now}'  AND 
+				 cars.idcar = {reservedCar.id_car}"""
 
-
-def check_id_car(idcar, ids1, reserve_outset, reserve_last):
-    check_id = []
-    check = (f"""SELECT cars.idcar, cars.modelcar, cars.yearcar
-                 FROM cars
-                 WHERE cars.idcar NOT IN {ids1} """)
+    list_true_false = []
     cursor.execute(check)
-    list_car_id = cursor.fetchall()
-    for res in list_car_id:
-        id = res[0]
-        idcarro = str(id)
-        check_id.append(idcarro)
+    list_car_reserve = cursor.fetchall()
+    for res in list_car_reserve:
+        if reservedCar.reserve_outset > res[0]:
 
-    if idcar in check_id:
-        print('encontrado')
-        return checkout(idcar, reserve_outset, reserve_last)
+            if reservedCar.reserve_outset > res[1]:
+                true = True
+                list_true_false.append(true)
+
+            elif reservedCar.reserve_outset < res[1]:
+                false = False
+                list_true_false.append(false)
+
+        elif reservedCar.reserve_outset < res[0]:
+
+            if reservedCar.reserve_last < res[0]:
+                true = True
+                list_true_false.append(true)
+
+            elif reservedCar.reserve_last > res[0]:
+                false = False
+                list_true_false.append(false)
+
+        elif reservedCar.reserve_outset == res[0]:
+            true = False
+            list_true_false.append(true)
+
+        elif reservedCar.reserve_last == res[1]:
+            false = False
+            list_true_false.append(false)
+
+    if False in list_true_false:
+        return{'msg': 'sem vaga'}
     else:
-        print(type(check_id))
-        print(type(idcar))
-        print(f'Não encontrado!{idcar},{check_id}')
-        return check_cars_in_reverse(reserve_outset, reserve_last)
+        
+        try:
+            registration = f"""INSERT INTO Reservedcar(idcar, reserveOutset, reserveLast) VALUES ({reservedCar.id_car}, '{reservedCar.reserve_outset}', '{reservedCar.reserve_last}');"""
+            cursor.execute(registration)
+            conn.commit()
+            return {'Reserva inicial': reservedCar.reserve_outset, 'Reserva Final': reservedCar.reserve_last, 'Id do carro escolhido': reservedCar.id_car}
+        except:
+            return {'Erro': 'Verificar dados de reserva (id)'}
 
 
-def check_id_car_option_two(idcar, ids1, reserve_outset, reserve_last):
-    check_id = []
-    check = (f"""select cars.idcar, cars.modelcar, cars.yearcar
-                                from cars
-                               WHERE cars.idcar <> {ids1}""")
+
+@app.post('/check_board')
+def check_board(cars: Cars):
+    car = []
+    check = f""" select * 
+                from cars 
+                where boardcar = '{cars.board}' """
     cursor.execute(check)
-    list_car_id = cursor.fetchall()
-    for res in list_car_id:
-        id = res[0]
-        idcarro = str(id)
-        check_id.append(idcarro)
-
-    if idcar in check_id:
-        print('encontrado')
-        return checkout(idcar, reserve_outset, reserve_last)
-    else:
-        print(f'Não encontrado, Informe o id novamente, por favor!')
-        return check_cars_in_reverse(reserve_outset, reserve_last)
+    list_car_reserve = cursor.fetchall()
+    for cars in list_car_reserve:
+        car.append(cars)
+    return car
 
 
-def check_id_car_option_three(idcar, reserve_outset, reserve_last):
-    check_id = []
-    check = (f"""SELECT cars.idcar, cars.modelcar, cars.yearcar
-                                       FROM cars""")
+
+
+@app.get('/listar_carro')
+def listar_carro():
+    car = []
+    check = """SELECT cars.idcar, cars.modelcar, cars.yearcar, cars.statecar
+                                       FROM cars 
+                                       order by  idcar  ASC """
     cursor.execute(check)
-    list_car_id = cursor.fetchall()
-    for res in list_car_id:
-        id = res[0]
-        idcarro = str(id)
-        check_id.append(idcarro)
-
-    if idcar in check_id:
-        print('encontrado')
-        return checkout(idcar, reserve_outset, reserve_last)
-    else:
-        print(f'Não encontrado, Informe o id novamente, por favor!')
-        return check_cars_in_reverse(reserve_outset, reserve_last)
+    list_car_reserve = cursor.fetchall()
+    for cars in list_car_reserve:
+        car.append(cars)
+    return car
 
 
-def final():
-    final = input('Deseja fazer voltar ao menu ou encerrar o atendimento? encerrar(e)/menu(m): ')
-    if final == 'm':
-        return main()
-    elif final == 'e':
-        return print('Até mais!')
+@app.post('/update_car')
+def update_state(cars: Cars):
+    
+    try:
+        registration = f"""update cars 
+                        set statecar = '{cars.state}'
+                        where boardcar = '{cars.board}'"""
+
+        cursor.execute(registration)
+        conn.commit()
+        car = []
+        check = f""" select * 
+                from cars 
+                where boardcar = '{cars.board}' """
+        cursor.execute(check)
+        list_car_reserve = cursor.fetchall()
+        for cars in list_car_reserve:
+            car.append(cars)
+
+        return car
+    except:
+        return {'msg': 'Não encontrado'}    
 
 
-def main():
-    user_main = input('Olá, Informe a letra correspondente ao que deseja fazer\n(c) Cadastrar um veículo\n(r) fazer uma reserva\nInforme a letra: ')
-    if user_main == 'c':
-        return car_registration()
-    elif user_main == 'r':
-        return reserve_car()
 
+@app.post('/delete_reserve')
+def delete_reserve(reservedCar: ReservedCar):
 
-main()
+    try:
+        registration = f"""delete from reservedcar 
+                        where  idreservation  = {reservedCar.id_car}"""
+        
+        cursor.execute(registration)
+        conn.commit()
+        return {'msg': 'deu certo'}                   
+    except:
+       return {'msg': 'deu errado'}   
